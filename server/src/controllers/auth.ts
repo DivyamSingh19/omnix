927f702166a99697cc6b61c986ee60998468b869
Divyam Singh <divyamsingh1918@gmail.com>
2025-06-25 22:13:54 +0530

    updateS

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,3 +1,20 @@
+import { Context } from "hono";
 import prisma from "../utils/prisma";
 
 
+async function loginUser(c:Context) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+async function registerUser(c:Context) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+export {loginUser,registerUser}
