465df58cfc50753586f8a66ad7eece026420213d
Divyam Singh <divyamsingh1918@gmail.com>
2025-06-29 13:55:08 +0530

    updates

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,5 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import { PrismaClient } from "@prisma/client/extension";
+import { withAccelerate } from "@prisma/extension-accelerate";
+ 
+const prisma = new PrismaClient({
+    datasourceurl : process.env.DATABASE_URL as string
+}).$extends(withAccelerate())
 export default prisma;
